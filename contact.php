d70da8a124a56daaa4f60dcd7310b5ec38e21ad8
parintontongrods <parinton.t@rmutsvmail.com>
2025-01-22 09:40:33 +0700

    แก้ไขไฟต์cotact.php ครั้งที่2

--- contact.php
+++ contact.php
@@ -6,8 +6,7 @@
                 <h1><center><font color="#FFFFFF">พรชัยอพาร์ตเมนต์</font></center></h1>
                 <?php include "Date.php"; ?>
             </td>
-            <td width="43" rowspan="2">&nbsp;</td>xxxxxxxxxxxxxxxx
-        </tr>
+            <td width="43" rowspan="2">&nbsp;</td>
         <tr>
             <td width="1007" bgcolor="#FFFFFF">
                 <img src="images/map.gif">
@@ -17,6 +16,7 @@
 				<li><a href="room.php">ห้องพัก&ราคา</li>
 				<li><a herf="guest.php">สมุดเยี่ยม</li>
 				<li><a href="contact.php">ติดต่อ</li>
+                <li><a href="contact.php">ขาย</li>
 				</ul><p>
 				<?php include "login_form.php";?>
 			</td>
